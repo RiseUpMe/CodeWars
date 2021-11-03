fbfb34946731ac6bcb0ed98fe79572898908a68c
RiseUpMe <zenek7@mail.ru>
2021-11-03 11:42:54 +0300

    n

---
+++ CW01.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function doubleChar(str) {
+    let doubleChar = "";
+    for (let a = 0; a < str.length; a++) {
+        doubleChar += str[a] + str[a];// Your code here
+    }
+    return doubleChar;
+}
