93d544ff7978d1e80170764751161265befdb2ca
RiseUpMe <zenek7@mail.ru>
2022-01-21 14:30:13 +0300

    m

---
+++ Reversed Strings.js
@@ -0,0 +1,9 @@
+const cannonsReady = (gunners) => {
+    for (var i in gunners) {
+        if (gunners[i] == "nay") {
+            return "Shiver me timbers!"
+        }
+    }
+    return "Fire!"
+
+}
