311dd88b9ce21cdfb80aa158724fe13e10f5ce5c
RiseUpMe <zenek7@mail.ru>
2022-02-01 16:41:25 +0300

    m

---
+++ Calculate BMI.js
@@ -0,0 +1,18 @@
+function bmi(weight, height) {
+    let index =  weight / Math.pow(height, 2);
+    if(index <= 18.5)  {
+        return "Underweight";
+    }
+
+    else if( (index <= 30.0) && index >25 ){
+        return "Overweight";
+    }
+    else if (index > 30.0 ){
+        return "Obese";
+    }
+
+    else if (index  <= 25.0 ){
+        return "Normal";
+
+    }
+}
